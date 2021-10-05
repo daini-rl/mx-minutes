e4d207a98353fe9d1eac2ca699265bcf6971ff7f
Daniel <danielramon117@gmail.com>
2021-10-04 20:43:32 -0500

    feat: create card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,14 @@
+interface CardProps {
+    title: string | JSX.Element | JSX.Element[];
+    children: JSX.Element | JSX.Element[];
+    isSecondary?: boolean;
+}
+
+export default function Card({ title, children, isSecondary }: CardProps) {
+    return (
+        <div className='w3-container w3-border-bottom w3-margin-bottom'>
+            {isSecondary ? <h3>{title}</h3> : <h2>{title}</h2>}
+            {children}
+        </div>
+    );
+}
