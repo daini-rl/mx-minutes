535fdb73dc2963c66de156a9ef70fff1cbe2f6c2
Daniel <danielramon117@gmail.com>
2021-10-05 20:01:33 -0500

    local: fix card implementations

--- src/sections/test/index.tsx
+++ src/sections/test/index.tsx
@@ -5,16 +5,24 @@ export default function Test() {
         <>
             <div className='w3-col l6'>
                 <Card title='Conditions'>
-                    <Card isSecondary title='First meal of the day?' children={<p>{'Time >'}</p>} />
-                    <Card isSecondary title='Medicine(s)' children={<p>{'Name >'}</p>} />
-                    <p>Add medicine</p>
-                    <p>Time it</p>
+                    <Card isSecondary title='First meal of the day?'>
+                        <p>{'Time >'}</p>
+                    </Card>
+                    <Card isSecondary title='Medicine(s)'>
+                        <p>{'Name >'}</p>
+                    </Card>
+                    <button>{'Add medicine'}</button>
+                    <button>{'Time it'}</button>
                 </Card>
             </div>
             <div className='w3-col l6'>
                 <Card title='Mx. Minutes says:'>
-                    <Card isSecondary title='First day:' children={<p>{'11:11'}</p>} />
-                    <Card isSecondary title='Next day:' children={<p>{'4:20'}</p>} />
+                    <Card isSecondary title='First day:'>
+                        <p>{'11:11'}</p>
+                    </Card>
+                    <Card isSecondary title='Next day:'>
+                        <p>{'4:20'}</p>
+                    </Card>
                 </Card>
             </div>
         </>
