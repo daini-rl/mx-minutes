628f6173ede972a0a3236dd36eb39d52ad41d885
Daniel <danielramon117@gmail.com>
2021-10-06 10:47:11 -0500

    fix: adjust padding in card

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 export default function Card({ title, children, isSecondary }: CardProps) {
     return (
-        <div className='w3-container w3-border-bottom w3-margin-bottom'>
+        <div className='w3-container w3-border-bottom w3-padding-16'>
             {isSecondary ? <h3>{title}</h3> : <h2>{title}</h2>}
             {children}
         </div>
