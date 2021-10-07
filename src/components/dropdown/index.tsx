9c670a5865da5581d2efb7864295280a6fd54011
Daniel RL <dramonl@pagatodo.com>
2021-10-07 18:02:54 -0500

    feat: create handle dropdown value function

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -11,26 +11,41 @@ export interface OptionItemI {
 interface DropdownI {
     defaultText?: string;
     options: Array<OptionItemI>;
+    handleDropdownValue: Function;
 }
 
-export default function Dropdown({ defaultText = 'Choose your option', options }: DropdownI) {
+export default function Dropdown({
+    defaultText = 'Choose your option',
+    options,
+    handleDropdownValue,
+}: DropdownI) {
+    const [selectedIndex, setSelectedIndex] = useState(-1);
     const [isOpen, setIsOpen] = useState(false);
 
-    const handleChange = () => {
+    const handleToogle = () => {
         setIsOpen(!isOpen);
+    };
+
+    const handleOptionItemClick = (index: number) => {
+        setSelectedIndex(index);
+        handleDropdownValue(options[index].value);
+        setIsOpen(false);
     };
 
     return (
         <div className={`w3-dropdown-click ${styles.dropdownContainer}`}>
-            <button onClick={handleChange} className='w3-button w3-white w3-border w3-round'>
-                {defaultText}
+            <button onClick={handleToogle} className='w3-button w3-white w3-border w3-round'>
+                {selectedIndex === -1 ? defaultText : options[selectedIndex].text}
                 <span className='w3-margin-left'>
                     <i className='fa fa-caret-down'></i>
                 </span>
             </button>
             <div className={`w3-dropdown-content w3-bar-block w3-border ${isOpen ? 'w3-show' : ''}`}>
                 {options?.map((option, index) => (
-                    <button key={index} className='w3-bar-item w3-button' value={option.value}>
+                    <button
+                        key={index}
+                        className='w3-bar-item w3-button'
+                        onClick={() => handleOptionItemClick(index)}>
                         {option.startItem && <span className='w3-margin-right'>{option.startItem}</span>}
                         {option.text}
                     </button>
