26b9b323456ad0307cb803040744d68eaae4ef21
Daniel RL <dramonl@pagatodo.com>
2021-10-08 14:44:52 -0500

    feat: create badge component

--- src/components/color-picker/ColorItem.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './colorItem.module.css';
-
-interface ColorItemI {
-    color: string;
-}
-
-export default function ColorItem({ color }: ColorItemI) {
-    return <i className={`w3-${color} w3-round-large ${styles.colorItem}`}></i>;
-}
