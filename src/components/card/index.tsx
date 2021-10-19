9c4fea66861d55a1307b0d2b8c54686642d9a178
Daniel RL <dramonl@pagatodo.com>
2021-10-19 18:41:21 -0500

    feat: change title prop in card component as optional

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,7 +1,7 @@
 import styles from './card.module.css';
 
 interface CardProps {
-    title: string | JSX.Element | JSX.Element[];
+    title?: string;
     children: JSX.Element | JSX.Element[];
     isSecondary?: boolean;
     buttons?: JSX.Element | JSX.Element[];
@@ -10,7 +10,7 @@ interface CardProps {
 export default function Card({ title, children, isSecondary, buttons }: CardProps) {
     return (
         <div className='w3-container w3-border-bottom w3-padding-16'>
-            {<h3 className={isSecondary ? '' : styles.primaryTitle}>{title}</h3>}
+            {title && (isSecondary ? <h3>{title}</h3> : <h3 className={styles.primaryTitle}>{title}</h3>)}
             {children}
             {buttons && <div className={styles.buttonsRow}>{buttons}</div>}
         </div>
