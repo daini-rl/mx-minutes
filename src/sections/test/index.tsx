9e3df9dbf3e8a17dabf24f5ae8038235c3db9144
Daniel RL <dramonl@pagatodo.com>
2021-10-20 18:24:53 -0500

    local: update card implementations

--- src/sections/test/index.tsx
+++ src/sections/test/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { Card, Badge, Dropdown, Button, MedicationCard, DayCard } from '../../components';
+import { Card, Badge, Dropdown, Button, MedicationCard, DayCard, Label } from '../../components';
 import { OptionItemI } from '../../components/dropdown';
 
 import { w3cssColors } from '../../utils/colors';
@@ -50,36 +50,54 @@ export default function Test() {
     return (
         <>
             <div className='w3-col l6'>
-                <Card title='Conditions:'>
-                    <Card isSecondary title='First meal of the day?'>
-                        <Dropdown
-                            defaultText='Choose an hour'
-                            options={mealHours}
-                            value={mealHour}
-                            handleDropdownValue={setMealHour}
-                        />
+                <Card>
+                    <Card isSecondary title='Conditions:'>
+                        <Card border>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'space-between',
+                                }}>
+                                <div>
+                                    <Label labelText='First meal of the day?' />
+                                </div>
+                                <div>
+                                    <Dropdown
+                                        defaultText='Choose an hour'
+                                        options={mealHours}
+                                        value={mealHour}
+                                        handleDropdownValue={setMealHour}
+                                    />
+                                </div>
+                            </div>
+                        </Card>
                     </Card>
                     <Card
                         isSecondary
-                        title='Medication(s)'
+                        title='Medication(s):'
                         buttons={
                             <>
                                 <Button buttonText={'Add medication'} buttonStateType='info' />
                                 <Button buttonText={'Time it'} buttonStateType='success' />
                             </>
                         }>
-                        <MedicationCard
-                            medicationsColors={medicationsColors}
-                            frequencyHours={frequencyHours}
-                            medicationStartHours={medicationStartHours}
-                            handleSaveMedication={handleSave}
-                        />
+                        <div className='multiple-cards-always-inside-a-div'>
+                            <MedicationCard
+                                medicationsColors={medicationsColors}
+                                frequencyHours={frequencyHours}
+                                medicationStartHours={medicationStartHours}
+                                handleSaveMedication={handleSave}
+                            />
+                        </div>
                     </Card>
                 </Card>
             </div>
             <div className='w3-col l6'>
-                <Card title='Mx. Minutes says:'>
-                    <DayCard />
+                <Card>
+                    <Card isSecondary title='Mx. Minutes says:'>
+                        <DayCard />
+                    </Card>
                 </Card>
             </div>
         </>
