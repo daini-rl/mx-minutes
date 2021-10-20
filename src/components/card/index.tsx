36b562849675e82149c0891b54d894ce044832d4
Daniel RL <dramonl@pagatodo.com>
2021-10-20 14:13:39 -0500

    feat: improve card component reusability

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,16 +1,25 @@
 import styles from './card.module.css';
 
 interface CardProps {
-    title?: string;
     children: JSX.Element | JSX.Element[];
+    title?: string;
     isSecondary?: boolean;
+    border?: boolean;
+    borderColor?: string;
     buttons?: JSX.Element | JSX.Element[];
 }
 
-export default function Card({ title, children, isSecondary, buttons }: CardProps) {
+export default function Card({ children, title, isSecondary, border, borderColor, buttons }: CardProps) {
     return (
-        <div className='w3-container w3-border-bottom w3-padding-16'>
-            {title && (isSecondary ? <h4>{title}</h4> : <h4 className={styles.primaryTitle}>{title}</h4>)}
+        <div
+            className={`w3-margin-bottom ${isSecondary ? styles.cardContainer : 'w3-padding-16'} ${
+                isSecondary ? '' : 'w3-container'
+            } ${
+                border
+                    ? `w3-round w3-border ${borderColor ? 'w3-border-' + borderColor : ''}`
+                    : 'w3-border-bottom'
+            }`}>
+            {title && <h4>{title}</h4>}
             {children}
             {buttons && <div className={styles.buttonsRow}>{buttons}</div>}
         </div>
