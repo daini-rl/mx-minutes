54f7b119e05201bac1c7933837d5048e3d2a90fb
Dani RL <danielramon117@gmail.com>
2021-10-21 16:22:19 -0500

    feat: create day card interfaces and implement it

--- src/components/dayCard/index.tsx
+++ src/components/dayCard/index.tsx
@@ -2,69 +2,59 @@ import { Card, Badge } from '../../components';
 
 import styles from './dayCard.module.css';
 
-export default function DayCard() {
+interface MedicationI {
+    // id: uuid ?
+    name: string;
+    color: string;
+    scheduledHours: Array<number>;
+}
+
+export interface MedicationDayI {
+    dayName: string;
+    medications: Array<MedicationI>;
+}
+
+interface DayCardI {
+    medicationDay: MedicationDayI;
+}
+
+export default function DayCard({ medicationDay }: DayCardI) {
+    const medicationHours = medicationDay.medications.flatMap(medication => medication.scheduledHours);
+    const medicationHoursFiltered = medicationHours.filter(
+        (medicationHour, index) => medicationHours.indexOf(medicationHour) === index
+    );
+    const medicationsGroupedByHours = medicationHoursFiltered.map(scheduledHour => {
+        const medicationGroup = {
+            scheduledHour,
+            medications: medicationDay.medications.filter(medication =>
+                medication.scheduledHours.includes(scheduledHour)
+            ),
+        };
+        return medicationGroup;
+    });
     return (
-        <>
-            <Card border cardClassName={styles.dayCardContainer}>
-                <h5 className='w3-center'>First Day</h5>
-                <div>
-                    <h6>
-                        <span className='w3-margin-right'>
-                            <i className='fa fa-clock-o'></i>
-                        </span>
-                        {'11:11'}
-                    </h6>
-                    <div className={styles.medicationsList}>
-                        <p>
-                            <Badge color={'orange'} />
-                            {'Tussin'}
-                        </p>
-                        <p>
-                            <Badge color={'indigo'} />
-                            {'Codeine'}
-                        </p>
-                        <p>
-                            <Badge color={'pink'} />
-                            {'Acetaminophen'}
-                        </p>
+        <Card border cardClassName={styles.dayCardContainer}>
+            <h5 className='w3-center'>{medicationDay.dayName}</h5>
+            <>
+                {medicationsGroupedByHours.map(medicationsGroup => (
+                    <div key={medicationsGroup.scheduledHour}>
+                        <h6>
+                            <span className='w3-margin-right'>
+                                <i className='fa fa-clock-o'></i>
+                            </span>
+                            {medicationsGroup.scheduledHour}
+                        </h6>
+                        <div className={styles.medicationsList}>
+                            {medicationsGroup.medications.map(medication => (
+                                <p key={medication.name}>
+                                    <Badge color={medication.color} />
+                                    {medication.name}
+                                </p>
+                            ))}
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <h6>
-                        <span className='w3-margin-right'>
-                            <i className='fa fa-clock-o'></i>
-                        </span>
-                        {'4:20'}
-                    </h6>
-                    <div className={styles.medicationsList}>
-                        <p>
-                            <Badge color={'indigo'} />
-                            {'Codeine'}
-                        </p>
-                        <p>
-                            <Badge color={'pink'} />
-                            {'Acetaminophen'}
-                        </p>
-                    </div>
-                </div>
-            </Card>
-            <Card border cardClassName={styles.dayCardContainer}>
-                <h5 className='w3-center'>Next Day</h5>
-                <div>
-                    <h6>
-                        <span className='w3-margin-right'>
-                            <i className='fa fa-clock-o'></i>
-                        </span>
-                        {'01:11'}
-                    </h6>
-                    <div className={styles.medicationsList}>
-                        <p>
-                            <Badge color={'orange'} />
-                            {'Tussin'}
-                        </p>
-                    </div>
-                </div>
-            </Card>
-        </>
+                ))}
+            </>
+        </Card>
     );
 }
